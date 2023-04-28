58828f3a74cbcf5d4b23219d2c123fde3e6da00b
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-28 15:19:53 -0500

    Created show views of each pokemon

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ app.get("/pokemon", function (req, res) {
 });
 
 app.get("/pokemon/:id", function (req, res) {
-  //   res.render("./Show", {
-  //second param must be an object
-  // pokemon: pokemon[req.params.id], //there will be a variable available inside the jsx file called fruit, its value is fruits[req.params.indexOfFruitsArray]
-  // });
-  res.send(req.params.id);
+  res.render("./Show", {
+    pokemon: pokemon[req.params.id],
+  });
+  //   res.send(req.params.id);
 });
 
 app.listen(port, () => {
