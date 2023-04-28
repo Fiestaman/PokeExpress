f47cd4eba6a0cdd3e35d359965bbee50296f62fc
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-28 14:50:08 -0500

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const pokemon = require("./models/pokemon");
 
 app.set("view engine", "jsx");
 app.set("views", "./views");
 app.engine("jsx", require("jsx-view-engine").createEngine());
 
-app.get("/pokemons", function (req, res) {
+app.get("/pokemon", function (req, res) {
   //   res.render("./Index", { pokemons: pokemons });
-  res.send("Welcome to the Pokemon App!");
+  res.send(pokemon);
 });
 
 app.listen(port, () => {
