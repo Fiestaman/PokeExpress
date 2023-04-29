7e4be269ab5c78089d77a2d68f18fdf3ed690332
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-29 09:50:21 -0500

    add New page

---
+++ views/New.jsx
@@ -0,0 +1,22 @@
+const React = require("react");
+const Nav = require("../components/Nav");
+
+class New extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>New Pokemon page</h1>
+        <form action="/pokemon" method="POST">
+          Name: <input type="text" name="name" />
+          <br />
+          Image: <input type="text" name="img" />
+          <br />
+          <input type="submit" name="" value="Create Pokemon" />
+        </form>
+        <Nav link="/pokemon" text="Back" />
+      </div>
+    );
+  }
+}
+
+module.exports = New;
